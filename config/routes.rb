dd2c23957a1b5c2864fac9e6d5eb4f9abd3d858b
Juan Felipe Ruiz <juanfeliperg2001@gmail.com>
2023-10-18 18:32:10 -0500

    Adds routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
       resources :user_parameterizations, only: [:create]
       resources :users, only: [:create]
       resources :authenticate, only: [:authenticate]
-      resources :community, only: [:index, :create, :update]
-      resources :community_posts, only: [:show, :create, :update]
+      resources :community_posts, only: [:show, :create]  
+      get 'show_related_posts', to: 'community_posts#show_related_posts'
       post 'authenticate', to: 'authentication#authenticate' # Utiliza 'post' para la acción de autenticación
     end
   end
