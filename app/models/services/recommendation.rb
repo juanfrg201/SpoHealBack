6ef552615fbe4b00d2a5025b0b3b2fe5838d6cca
Juan Felipe Ruiz <juanfeliperg2001@gmail.com>
2023-10-25 17:11:33 -0500

    Modifies recomendation System

--- app/models/services/recommendation.rb
+++ app/models/services/recommendation.rb
@@ -18,17 +18,12 @@ module Services
 
     private
 
-    def recommend_content_based
-      activity_type = @user_parameterization.activity.activity_type
-      favorite_intensity = @user_parameterization.activity.intensity
-      favorite_duration = @user_parameterization.activity.duration
-      activities = (activity_type.activities + Activity.where(intensity: favorite_intensity) + Activity.where(duration: favorite_duration)).uniq
-
-
+    def recommend_content_based(intensity, duration)
+      activity_desease_filter = take_activities_deseas(@user_parameterization)
+      
+      activities = (activity_desease_filter.where(intensity: 0) + activity_desease_filter.where(duration: duration)).uniq
       sorted_activities = activities_with_fitness(activities).sort_by { |activity| -activity[:fitness_score] }
-
       content_based_recommendations = sorted_activities.map { |activity| activity[:activity] }
-
       content_based_recommendations
     end
 
@@ -42,12 +37,9 @@ module Services
         activities << ActivityRecommendation.find(data[:item_id]).activity
       end
 
-      activities_with_fitness(activities)
-
-      sorted_activities = activities_with_fitness(activities).sort_by { |activity| -activity[:fitness_score] }
-
+      activityes_filtered = filter_activity_colaborative_deseas(activities, @user_parameterization)
+      sorted_activities = activities_with_fitness(activityes_filtered).sort_by { |activity| -activity[:fitness_score] }
       collaborative_recommendations = sorted_activities.map { |activity| activity[:activity] }
-
       collaborative_recommendations
     end
 
@@ -66,6 +58,47 @@ module Services
       fitness_score = (ibm.to_i * 0.4) + (activity_intensity * 0.3) + (activity_duration * 0.3)
 
       fitness_score
+    end
+
+    def take_activities_deseas(user_parameterization)
+      Activity.where(
+        sport_medical_restriccion: user_parameterization.sport_medical_restriccion,
+        sport_muscle_pains: user_parameterization.sport_muscle_pains,
+        general_pain: user_parameterization.general_pain,
+        is_hipertension: user_parameterization.is_hipertension,
+        is_asthma: user_parameterization.is_asthma,
+        is_chest_pain: user_parameterization.is_chest_pain,
+        pain_cardiac: user_parameterization.pain_cardiac,
+        cardiac_family_pain: user_parameterization.cardiac_family_pain,
+        cholesterol_pain: user_parameterization.cholesterol_pain,
+        dizzines_pain: user_parameterization.dizzines_pain,
+        smoke_pain: user_parameterization.smoke_pain,
+        covid_19: user_parameterization.covid_19
+      )
+    end
+
+    def filter_activity_colaborative_deseas(activities, user_parameterization)
+      matching_activities = []
+
+      activities.each do |activity|
+        # Compara los campos de la actividad con los campos en los parámetros del usuario
+        if !activity.sport_medical_restriccion == user_parameterization.sport_medical_restriccion &&
+          !activity.sport_muscle_pains == user_parameterization.sport_muscle_pains &&
+          !activity.general_pain == user_parameterization.general_pain &&
+          !activity.is_hipertension == user_parameterization.is_hipertension &&
+          !activity.is_asthma == user_parameterization.is_asthma &&
+          !activity.is_chest_pain == user_parameterization.is_chest_pain &&
+          !activity.pain_cardiac == user_parameterization.pain_cardiac &&
+          !activity.cardiac_family_pain == user_parameterization.cardiac_family_pain &&
+          !activity.cholesterol_pain == user_parameterization.cholesterol_pain &&
+          !activity.dizzines_pain == user_parameterization.dizzines_pain && 
+          !activity.smoke_pain == user_parameterization.smoke_pain &&
+          !activity.covid_19 == user_parameterization.covid_19
+          matching_activities << activity
+        end
+      end
+
+      matching_activities
     end
   end
 end
