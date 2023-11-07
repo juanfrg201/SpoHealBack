6fd65a3d369706fab64f7f6dc9eb1b2b77765710
Juan Felipe Ruiz <juanfeliperg2001@gmail.com>
2023-11-06 20:06:28 -0500

    Adds BD changes

--- app/controllers/api/v1/user_parameterizations_controller.rb
+++ app/controllers/api/v1/user_parameterizations_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::UserParameterizationsController < ApplicationController
   def create
+    puts "**********"
+    puts user_parameterization_params[:auth_token]
     user = User.find(user_parameterization_params[:auth_token]) 
 
     user_parameterization = UserParameterization.new(user_id: user.id, activity_id: Activity.last.id, cardiovascular_desease_id: CardiovascularDesease.last.id, age: user_parameterization_params[:age], weight: user_parameterization_params[:weight], height: user_parameterization_params[:height], sport_medical_restriccion: user_parameterization_params[:sportMedicalRestriction], sport_muscle_pains: user_parameterization_params[:sportMusclePains], general_pain: user_parameterization_params[:generalPain], is_hipertension: user_parameterization_params[:isHypertension], is_asthma: user_parameterization_params[:isAsthma], is_chest_pain: user_parameterization_params[:isChestPain], pain_cardiac: user_parameterization_params[:painCardiac], cardiac_family_pain: user_parameterization_params[:cardiacFamilyPain], cholesterol_pain: user_parameterization_params[:cholesterolPain], dizzines_pain: user_parameterization_params[:dizzinessPain], smoke_pain: user_parameterization_params[:smokePain], covid_19: user_parameterization_params[:covid19])
